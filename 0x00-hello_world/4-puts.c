7e2b9b76126f82d07933e8f622dcf2598b3f583f
Dtwitchy <destiny.aghafekokhian200@gmail.com>
2023-09-07 18:40:43 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+4-puts.c
+/**
+ * main - Entry point
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+puzzle");
+        return (0);
+}
