b4675fed9d26c94f78a52198885f8312e4a91ef5
Dtwitchy <destiny.aghafekokhian200@gmail.com>
2023-09-11 11:16:44 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-	char i;
+	char c;
 
-	for (i = 'a'; i <= 'z'; i++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		if (i != 'e' && i != 'q')
-			putchar(i);
-        }
+		if (c != 'e' && c != 'q')
+			putchar(c);
+	}
 	putchar('\n');
 	return (0);
 }
