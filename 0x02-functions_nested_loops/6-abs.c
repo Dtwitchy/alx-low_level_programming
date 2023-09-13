733681b743ac14f637dadf183a00c80a8c6c4b46
Dtwitchy <destiny.aghafekokhian200@gmail.com>
2023-09-13 07:01:17 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,11 +8,8 @@
 int _abs(int n)
 {
 	if (n >= 0)
-                return (-n);
-        else if (n >= 0)
-        {
-                return (n);
-        }
-
-	return (0);
+	{
+		return (n);
+	}
+	return (-n);
 }
