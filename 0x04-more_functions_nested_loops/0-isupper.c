3ac49e36f28cb144c5ac2b203b578bc180a2bbe9
Dtwitchy <destiny.aghafekokhian200@gmail.com>
2023-09-14 15:06:20 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,13 @@
 #include "main.h"
 /**
- * _isupper - detect upper case alphabets
- *
- * @c: input character
- *
- * Return: (1) for true (0) false
- *
- */
+* _isupper - detect upper case alphabets
+*
+* @c: input character
+*
+* Return: (1) for true (0) false
+*
+*/
 int _isupper(int c)
 {
-	if (c == 'A' && c <= 'z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+return (C >= '0' && C <= '9');
 }
